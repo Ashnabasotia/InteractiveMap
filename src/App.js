90d53280b4297ae71ad6fa1a20c4e24b17e40996
Ashna Basotia <54627882+Ashnabasotia@users.noreply.github.com>
2023-07-27 05:20:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MapWithBubbles from './mapWithMarkers';
+
+function App() {
+  return (
+    <div className="App">
+    <MapWithBubbles />
+    </div>
+  );
+}
+
+export default App;
